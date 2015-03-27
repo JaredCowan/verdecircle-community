c2fef769091b9db5602e299f1e88f7fa3363bda9
JaredCowan <jaredlc@outlook.com>
2015-03-26 19:15:03 -0700

    added style for main post index

--- app/assets/javascripts/verdecircle/posts.js
+++ app/assets/javascripts/verdecircle/posts.js
@@ -6,17 +6,24 @@ $(function(e) {
         $content = $(".popover .popover-content"),
         username = $this.data("user-name"),
         userId   = $this.data("user-id"),
+        postId   = $this.data("post-id"),
         cardId   = $this.attr("id");
+        loggedIn = (window.currentuser == undefined)
 
     $.ajax({
       type: 'GET',
-      url: Routes.profile_page_path(username),
+      url: Routes.post_path(postId),
       dataType: 'json',
       error: function() {
         $content.html("Sorry, there was an error loading profile card for '" + username + "'.");
       },
       success: function(response) {
-        var temp = "<h3>" + username + "</h3> <br> <img src='" + response['image_url'] + "'>";
+        if (!loggedIn) {
+          var followButton = "<br><hr> <a href='javascript:;' class='btn btn-primary'>Follow</a>";
+        } else {
+          var followButton = "";
+        }
+        var temp = "<h3>" + username + "</h3> <br> <img src='" + response['user']['image_url'] + "'>" + followButton;
         $this.addClass("loaded");
         $content.html(temp);
       }
