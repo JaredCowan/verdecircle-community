d52c2b186dff768039c63ecf509a52fbeeaab54f
JaredCowan <jaredlc@outlook.com>
2015-04-01 00:51:17 -0700

    changed the username hover card

--- app/assets/javascripts/verdecircle/posts.js
+++ app/assets/javascripts/verdecircle/posts.js
@@ -1,35 +1,5 @@
 $(function(e) {
-
-  // $("[data-toggle='popover']").on("shown.bs.popover", function(e) {
-  //   var $this    = $(this),
-  //       $popover = $(".popover"),
-  //       $content = $(".popover .popover-content"),
-  //       username = $this.data("user-name"),
-  //       userId   = $this.data("user-id"),
-  //       postId   = $this.data("post-id"),
-  //       cardId   = $this.attr("id");
-  //       loggedIn = (window.currentuser == undefined)
-
-  //   $.ajax({
-  //     type: 'GET',
-  //     url: Routes.post_path(postId),
-  //     dataType: 'json',
-  //     error: function() {
-  //       $content.html("Sorry, there was an error loading profile card for '" + username + "'.");
-  //     },
-  //     success: function(response) {
-  //       if (!loggedIn) {
-  //         var followButton = "<br><hr> <a href='javascript:;' class='btn btn-primary'>Follow</a>";
-  //       } else {
-  //         var followButton = "";
-  //       }
-  //       var temp = "<h3>" + username + "</h3> <br> <img src='" + response['user']['image_url'] + "'>" + followButton;
-  //       $this.addClass("loaded");
-  //       $content.html(temp);
-  //     }
-  //   });
-  // });
-
+  
   $("[data-hovercard]").on("mouseover", function(e) {
     var $this = $(this);
     // console.log($this);
@@ -65,9 +35,6 @@ $(function(e) {
         $popover = $(".popover"),
         $content = $(".popover .popover-content"),
         username = $this.data("hovercard"),
-        // userId   = $this.data("user-id"),
-        // postId   = $this.data("post-id"),
-        // cardId   = $this.attr("id"),
         loggedIn = (window.currentuser == undefined);
 
     $.ajax({
