ddb9ba46082ca19fe8e57e9855084b1112824277
JaredCowan <jaredlc@outlook.com>
2015-04-20 18:03:56 -0700

    added option to select yearly or monthly pricing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -86,6 +86,36 @@ var ready = function() {
         $this[0].rows += Math.floor(v.length / ($this.width() / 6.7825));
       });
   });
+
+  $(".pricing-switcher label").on("click", function(e) {
+    var $this           = $(this),
+        $switchBtn      = $(".price-switch-btn"),
+        $monthlyInput   = $("#monthly:checked"),
+        $yearlyInput    = $("#yearly:checked"),
+        $monthlyPricing = $(".pricing-head > .monthly-pricing"),
+        $yearlyPricing  = $(".pricing-head > .yearly-pricing"),
+        $pricingTable   = $(".pricing-table");
+
+    switch($this[0].previousElementSibling.id) {
+      case "monthly":
+        $switchBtn.addClass("monthly").removeClass("yearly");
+        $pricingTable.addClass("monthly").removeClass("yearly");
+        setTimeout(function() {
+          $yearlyPricing.hide();
+          $monthlyPricing.show();
+        }, 170);
+        break;
+      case "yearly":
+        $pricingTable.removeClass("monthly").addClass("yearly");
+        $switchBtn.removeClass("monthly").addClass("yearly");
+        setTimeout(function() {
+          $monthlyPricing.hide();
+          $yearlyPricing.show();
+        }, 170);
+        break;
+    }
+    // console.log($yearlyPricing);
+  });
 }
 
 
