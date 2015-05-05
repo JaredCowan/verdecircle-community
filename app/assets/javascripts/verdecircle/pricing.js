13f2f1755c7b22459f97407df2c3b20dc1951cf2
JaredCowan <jaredlc@outlook.com>
2015-05-05 16:03:42 -0700

    fixed tabindex on pricing

--- app/assets/javascripts/verdecircle/pricing.js
+++ app/assets/javascripts/verdecircle/pricing.js
@@ -26,3 +26,33 @@ $(".pricing-switcher label").on("click", function(e) {
       break;
   }
 });
+
+// For screenreaders
+$(".pricing-switcher input").on("click", function(e) {
+  var $this           = $(this),
+      $switchBtn      = $(".price-switch-btn"),
+      $monthlyInput   = $("#monthly:checked"),
+      $yearlyInput    = $("#yearly:checked"),
+      $monthlyPricing = $(".controller__pages .monthly-pricing"),
+      $yearlyPricing  = $(".controller__pages .yearly-pricing"),
+      $pricingTable   = $(".pricing-table");
+
+  switch($this[0].id) {
+    case "monthly":
+      $switchBtn.addClass("monthly").removeClass("yearly");
+      $pricingTable.addClass("monthly").removeClass("yearly");
+      setTimeout(function() {
+        $yearlyPricing.hide();
+        $monthlyPricing.show();
+      }, 170);
+      break;
+    case "yearly":
+      $pricingTable.removeClass("monthly").addClass("yearly");
+      $switchBtn.removeClass("monthly").addClass("yearly");
+      setTimeout(function() {
+        $monthlyPricing.hide();
+        $yearlyPricing.show();
+      }, 170);
+      break;
+  }
+});
