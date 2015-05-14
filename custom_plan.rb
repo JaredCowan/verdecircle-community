f57e3d8be5eab4eeef283bd3cc180dccdbd40866
JaredCowan <jaredlc@outlook.com>
2015-05-14 15:30:51 -0700

    added license

--- custom_plan.rb
+++ custom_plan.rb
@@ -1,10 +1,8 @@
+# https://github.com/burke/zeus/blob/master/docs/ruby/modifying.md
+
 require 'zeus/rails'
 
 class CustomPlan < Zeus::Rails
-
-  # def my_custom_command
-  #  # see https://github.com/burke/zeus/blob/master/docs/ruby/modifying.md
-  # end
 
   def test
     require 'simplecov'
